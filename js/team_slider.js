eaf15e94274bd9effe5af4387bf2871e8f0d49e9
legionerium <legionerium@gmail.com>
2019-09-17 19:42:46 +0300

    Update team_slider.js
    fix name slide

--- js/team_slider.js
+++ js/team_slider.js
@@ -1,12 +1,27 @@
 window.onload = function () {
-	
+	var i = 0;
 	teamSlider(0);
 
 	var team_slide_button = document.getElementsByClassName('team_slide_button')[0].children;
-	team_slide_button.addEventListener('click', function(event){
-		if (event.target == team_slide_button[0]) {
-			teamSlider(1);
-		}
+	team_slide_button[0].addEventListener("click", function(event){
+			event.preventDefault()
+				++i
+				if (i<=2) {
+					teamSlider(i);
+				}else{
+					i=0;
+					teamSlider(i);
+				}
+	}, false)
+	team_slide_button[1].addEventListener("click", function(event){
+			event.preventDefault()
+				if (i==0) {
+					i=2;
+					teamSlider(i);
+				}else{
+					--i;
+					teamSlider(i);
+				}
 	}, false)
 }
 
@@ -47,12 +62,15 @@ function teamSlider(x){
 
 	var team_item_name = document.getElementsByClassName('discrition');
 	var person_ava = document.getElementsByClassName('person_ava');
+	var team_slide_name = document.getElementsByClassName('team_slide_name')[0];
 	var i = 0;
 	var j = 0;
 	for (var key in team[x]){
+		//присвоение значений первым трем дочерным элементам класса "discrition"
 		if (i<3) {
 			team_item_name[0].children[i].innerHTML = team[x][key];
 			++i;
+		//присвоение значений 4 и 5 дочерным элементам класса "discrition"
 		}else{
 			if (3<=i && i<6) {					
 				let temp = team[x][key];
@@ -61,19 +79,29 @@ function teamSlider(x){
 				++j;
 				++i;
 			}else{
-				let temp = team[x+1][key];				
-				let elem = team_item_name[0].children[4].children[0].children;
-				elem[0].setAttribute("src", temp);
 				for (var i = 0; i < person_ava.length; i++) {
 					let temp = team[x][key]
 					person_ava[i].setAttribute("src", temp);
+				}if (x+1>=team.length){
+					let temp = team[0][key];				
+					let elem = team_item_name[0].children[4].children[0].children;
+					elem[0].setAttribute("src", temp);
+				}else{
+					let temp = team[x+1][key];				
+					let elem = team_item_name[0].children[4].children[0].children;
+					elem[0].setAttribute("src", temp);
 				}
 			}
 		}
+	}
+	// перебор имен следующего слайда
+	for (var key in team[x]){
+		if (x+1>=team.length) {
+			team_slide_name.innerHTML = team[0][key];
+			break
+		}else{
+			team_slide_name.innerHTML = team[x+1][key];
+			break
+		}
 	}		
 }
-
-
-// var team_slide_btn = document.getElementsByClassName('team_slide_button')[0].children;
-// team_slide_btn.addEventListener('click', function(event){
-
